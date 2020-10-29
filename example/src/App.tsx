4ef827f8597f7a22f41344db85dafca8a959698d
gtindo <gtyletindo@gmail.com>
2020-10-29 02:51:57 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-lists-dnd'
+import 'react-lists-dnd/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
